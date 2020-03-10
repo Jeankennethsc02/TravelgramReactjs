17fe9cc59f70aea5e37625a6674a5f772cac7b2f
Jeankennethsc02 <staclarajeankeenth@gmail.com>
2020-03-11 01:34:13 +0800

    Updates

---
+++ precache-manifest.29de6538999edd019a1de25cc262bb59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fb471d1f96c3cf62ad47dcb40ae65a",
+    "url": "/TravelgramReactjs/index.html"
+  },
+  {
+    "revision": "4aa589981e01be0ae3bf",
+    "url": "/TravelgramReactjs/static/css/main.4b5c3934.chunk.css"
+  },
+  {
+    "revision": "051d57c13f18751d0dc8",
+    "url": "/TravelgramReactjs/static/js/2.ff830c0b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TravelgramReactjs/static/js/2.ff830c0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4aa589981e01be0ae3bf",
+    "url": "/TravelgramReactjs/static/js/main.2f074cd0.chunk.js"
+  },
+  {
+    "revision": "9c694283b96c88998dd1",
+    "url": "/TravelgramReactjs/static/js/runtime-main.92b4618d.js"
+  }
+]);
